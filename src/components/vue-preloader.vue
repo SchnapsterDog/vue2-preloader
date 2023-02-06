5b46a5c5798aa790855aae4e5aafeb05195c38e6
SchnapsterDog <olivertrajceski@yahoo.com>
2023-02-06 21:25:48 +0100

    feat: improve stylesheet, added moduled css

--- src/components/vue-preloader.vue
+++ src/components/vue-preloader.vue
@@ -1,75 +1,81 @@
 <template>
-	<div class="preloader" :style="preloaderStyle">
-		<div v-if="showPercent" class="percent">{{ width }} %</div>
-		<div v-if="showBar" class="bar">
-			<div class="barconfirm" ref="barconfirm" />
-		</div>
-	</div>
+  <div
+    :class="$style.preloader"
+    :style="preloaderStyle"
+  >
+    <div
+      v-if="width < 100"
+      :class="$style.percentBar"
+    >
+      {{ width }} %
+    </div>
+    <div
+      v-if="width < 100"
+      :class="$style.loadingWrapper"
+    >
+      <div :class="$style.loadingBar" ref="loadingbar" />
+    </div>
+  </div>
 </template>
 <script>
 export default {
-	name: "VuePreloader",
-	data() {
-		return {
-			showBar: true,
-			showPercent: true,
-			width: 0,
-		};
-	},
-	computed: {
-		preloaderStyle() {
-			return this.width >= 100 ? "width: 0%" : "";
-		},
-	},
-	watch: {
-		width: {
-			handler(width) {
-				if (width < 100) {
-					setTimeout(() => {
-						this.width = width += 1;
-						this.$refs.barconfirm.style.width = this.width + "%";
-					}, 15);
-				} else {
-					this.showBar = false;
-					this.showPercent = false;
-				}
-			},
-		},
-	},
-	mounted() {
-		this.width = this.width += 1;
-	},
+  name: "VuePreloader",
+  data() {
+    return {
+      width: 0
+    };
+  },
+  computed: {
+    preloaderStyle() {
+      return this.width >= 100 ? "width: 0%" : "";
+    },
+  },
+  watch: {
+    width: {
+      handler(width) {
+        if (width < 100) {
+          setTimeout(() => {
+            this.width = width += 1;
+            this.$refs.loadingbar.style.width = this.width + "%";
+          }, 15);
+        }
+      },
+    },
+  },
+  mounted() {
+    this.width = this.width += 1;
+  }
 };
 </script>
-<style lang="css">
+<style lang="css" module>
 .preloader {
-	position: fixed;
-	display: flex;
-	top: 0;
-	left: 0;
-	align-items: center;
-	justify-content: center;
-	width: 100%;
-	height: 100vh;
-	z-index: 100;
-	background-color: #091a28;
-	flex-direction: column;
-	transition: all 1.4s ease-in-out;
+  height: 100vh;
+  width: 100%;
+  top: 0;
+  left: 0;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #091a28;
+  transition: all 1.4s ease-in-out;
+  z-index: 100;
 }
 
-.percent {
-	color: white;
-	transition: all 0.2s ease-in-out;
+.percentBar {
+  color: white;
+  transition: all 0.2s ease-in-out;
 }
 
-.bar {
-	width: 60%;
-	margin-top: 20px;
+.loadingWrapper {
+  width: 60%;
+  margin-top: 20px;
 }
 
-.barconfirm {
-	width: 1%;
-	padding: 1px 0;
-	background-color: white;
+.loadingBar {
+  width: 1%;
+  padding: 1px 0;
+  background-color: white;
 }
 </style>
